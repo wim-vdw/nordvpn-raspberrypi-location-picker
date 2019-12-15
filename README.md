370df128326db0a29f8ec306515e94a19a0c5054
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2019-12-16 00:03:16 +0100

    update README file with other examples

--- README.md
+++ README.md
@@ -9,19 +9,19 @@ Script has been tested on Raspbian GNU/Linux 10 (buster).
 [How to setup OpenVPN on Raspberry Pi | NordVPN](https://nordvpn.com/tutorials/raspberry-pi/openvpn/)
 ## Prerequisites
 Install OpenVPN and the NordVPN ovpn files according to the instructions mentioned in the link above.  
-Prepare a file /etc/openvpn/nordvpnauth.txt including your NordVPN credentials (first line email address, second line password).  
+Prepare a file `/etc/openvpn/nordvp-nauth.txt` including your NordVPN credentials (first line email address, second line password).  
 Also make sure the user who will execute the script has sudo rights.
 ## Installation instructions
 Put the bash script on your Raspberry Pi or clone this repository.  
 Make sure the user who executes the script has sudo rights (example pi user).
 ## Usage
-Help screen:
+Help screen of the script:
 ```
 NordVPN Raspberry Pi random location picker.
 
 Details for some of the arguments:
-  CC    - NordVPN Country Code in lowercase.
-  PROTO - NordVPN Transfer protocol lowercase (tcp or udp).
+  [CC]    - NordVPN Country Code in lowercase.
+  [PROTO] - NordVPN Transfer protocol in lowercase (tcp or udp).
 
 Usage: nordvpn_pi.sh [OPTIONS] COMMAND [ARGS]
 
@@ -38,12 +38,20 @@ Commands:
   start [CC] [PROTO]   Start OpenVPN process for a NordVPN location and protocol.
                        If a process already runs nothing will happen.
   reload [CC] [PROTO]  Reload OpenVPN process for a NordVPN location and protocol.
-                       Current process (if running) will be stopped, a new one will be started.
+                       Current process will be stopped, a new one will be started.
 ```
-## Example
+## Examples
 Start a new OpenVPN process for a random location in Belgium (be) using NordVPN's TCP/IP protocol:
 ```
-$ nordvpn_pi.sh be tcp
+$ nordvpn_pi.sh start be tcp
+```
+Other examples:
+```
+$ nordvpn_pi.sh countries
+$ nordvpn_pi.sh protocols
+$ nordvpn_pi.sh ip
+$ nordvpn_pi.sh check
+$ nordvpn_pi.sh kill
 ```
 ## Task list
 - [x] Add feature to display NordVPN's supported country codes and protocols.
--- nordvpn_pi.sh
+++ nordvpn_pi.sh
@@ -9,6 +9,7 @@
 #   10 - No country codes could be displayed.
 #   20 - No files for country.
 #   30 - Problem killing OpenVPN process.
+#   40 - OpenVPN process already running.
 #
 # Change history:
 #   1.0.0 - Initial version.
@@ -17,15 +18,28 @@
 #   1.1.2 - Update help screen (start and reload options).
 ###################################################################################################
 
+# Test:
+# sudo openvpn --config /etc/openvpn/de705.nordvpn.com.tcp443.ovpn --auth-user-pass /etc/openvpn/nordvpn-auth.txt --connect-timeout 5 --connect-retry-max 3
+
 VERSION="1.1.2"
 BASEPATH="/etc/openvpn"
+AUTH_FILE="/etc/openvpn/nordvpn-auth.txt"
+CONNECT_TIMEOUT=3
+CONNECT_RETRY_MAX=3
+
+# Exit codes.
+SUCCESS=0
+NO_COUNTRY_CODES=10
+NO_FILES_FOR_COUNTRY=20
+PROBLEM_KILLING_PROCESS=30
+ALREADY_RUNNING=40
 
 display_help() {
   echo "NordVPN Raspberry Pi random location picker."
   echo
   echo "Details for some of the arguments:"
-  echo "  CC    - NordVPN Country Code in lowercase."
-  echo "  PROTO - NordVPN Transfer protocol lowercase (tcp or udp)."
+  echo "  [CC]    - NordVPN Country Code in lowercase."
+  echo "  [PROTO] - NordVPN Transfer protocol in lowercase (tcp or udp)."
   echo
   echo "Usage: nordvpn_pi.sh [OPTIONS] COMMAND [ARGS]"
   echo
@@ -42,7 +56,7 @@ display_help() {
   echo "  start [CC] [PROTO]   Start OpenVPN process for a NordVPN location and protocol."
   echo "                       If a process already runs nothing will happen."
   echo "  reload [CC] [PROTO]  Reload OpenVPN process for a NordVPN location and protocol."
-  echo "                       Current process (if running) will be stopped, a new one will be started."
+  echo "                       Current process will be stopped, a new one will be started."
 }
 
 display_version() {
@@ -53,7 +67,7 @@ display_countries() {
   if ! ls -l "$BASEPATH"/*nordvpn*ovpn >/dev/null 2>&1
   then
     echo "Error: Make sure NordVPN ovpn files are located in /etc/openvpn"
-    exit 10
+    exit $NO_COUNTRY_CODES
   fi
   COUNTRY_PREVIOUS=""
   echo "The following country codes are supported:"
@@ -80,21 +94,21 @@ display_public_ip() {
 }
 
 check_openvpn_process() {
-  if ! ps -ef | pgrep openvpn | grep -v pgrep >/dev/null 2>&1
+  if ! ps -ef | pgrep openvpn >/dev/null 2>&1
   then
     echo "OpenVPN process status for NordVPN: not active"
   else
-    PROCESS_ID=$(ps -ef | pgrep openvpn | grep -v pgrep)
+    PROCESS_ID=$(ps -ef | pgrep openvpn)
     echo "OpenVPN process status for NordVPN: active"
     echo "OpenVPN process id: $PROCESS_ID"
   fi
 }
 
 kill_current_connection() {
-  if ! ps -ef | pgrep openvpn | grep -v pgrep >/dev/null 2>&1
+  if ! ps -ef | pgrep openvpn >/dev/null 2>&1
   then
     echo "There is currently no OpenVPN process for NordVPN running."
-    exit 0
+    exit $SUCCESS
   fi
   echo "Killing NordVPN connection now!"
   if sudo killall openvpn >/dev/null 2>&1
@@ -103,11 +117,19 @@ kill_current_connection() {
   else
     echo "Error: Problem killing OpenVPN process for NordVPN."
     echo "Check your sudo rights."
-    exit 30
+    exit $PROBLEM_KILLING_PROCESS
   fi
 }
 
 start_connection() {
+  if ps -ef | pgrep openvpn >/dev/null 2>&1
+  then
+    PROCESS_ID=$(ps -ef | pgrep openvpn)
+    echo "Error: OpenVPN process for NordVPN already running."
+    echo "OpenVPN process id: $PROCESS_ID"
+    echo "Use the kill command of this script first or the reload command."
+    exit $ALREADY_RUNNING
+  fi
   echo "Starting new connection..."
 }
 
@@ -121,7 +143,7 @@ get_random_file() {
   if ! ls -l "$BASEPATH"/"$COUNTRY"*nordvpn*"$PROTOCOL"*ovpn >/dev/null 2>&1
   then
     echo "Error: No files found, make sure you specify an existing country and protocol."
-    exit 20
+    exit $NO_FILES_FOR_COUNTRY
   fi
   FILES=()
   COUNTER=0
@@ -138,63 +160,63 @@ get_random_file() {
 if [[ $# -eq 0 ]]
 then
   display_help
-  exit 0
+  exit $SUCCESS
 fi
 
 if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]
 then
   display_help
-  exit 0
+  exit $SUCCESS
 fi
 
 if [[ $1 == "-v" ]] || [[ $1 == "--version" ]]
 then
   display_version
-  exit 0
+  exit $SUCCESS
 fi
 
 if [[ $1 == "countries" ]]
 then
   display_countries
-  exit 0
+  exit $SUCCESS
 fi
 
 if [[ $1 == "protocols" ]]
 then
   display_protocols
-  exit 0
+  exit $SUCCESS
 fi
 
 if [[ $1 == "ip" ]]
 then
   display_public_ip
-  exit 0
+  exit $SUCCESS
 fi
 
 if [[ $1 == "check" ]]
 then
   check_openvpn_process
-  exit 0
+  exit $SUCCESS
 fi
 
 if [[ $1 == "kill" ]]
 then
   kill_current_connection
-  exit 0
+  exit $SUCCESS
 fi
 
 if [[ $1 == "start" ]]
 then
   start_connection
-  exit 0
+  exit $SUCCESS
 fi
 
 if [[ $1 == "reload" ]]
 then
   reload_connection
-  exit 0
+  exit $SUCCESS
 fi
 
 echo "Nothing done."
 echo "Command for help: nordvpn_pi.sh --help"
-exit 0
+exit $SUCCESS
